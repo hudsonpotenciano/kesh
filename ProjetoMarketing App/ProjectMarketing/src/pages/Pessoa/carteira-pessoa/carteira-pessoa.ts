9b7c521fdfc14710ab94769cbd5229e176721c49
hudsonp.developer@gmail.com <hudsonp.developer@gmail.com>
2020-03-22 17:37:31 -0300

    alteracoes

--- ProjetoMarketing App/ProjectMarketing/src/pages/Pessoa/carteira-pessoa/carteira-pessoa.ts
+++ ProjetoMarketing App/ProjectMarketing/src/pages/Pessoa/carteira-pessoa/carteira-pessoa.ts
@@ -4,7 +4,6 @@ import { PessoaProvider } from '../../../providers/pessoa/pessoa';
 import { DadosPessoaEmpresa, PessoaLoja } from '../../../models/pessoa.model';
 import { StoragePessoaProvider } from '../../../providers/storage/storage-pessoa';
 import { EmpresaProvider } from '../../../providers/empresa/empresa';
-import { Perfil } from '../../../models/empresa.model';
 
 @IonicPage()
 @Component({
@@ -34,17 +33,6 @@ export class CarteiraPessoaPage {
   obtenhaDados(refresh = undefined) {
     this.pessoaProvider.obtenhaDadosPessoaLojas(refresh !== undefined)
       .then((resultado: PessoaLoja[]) => {
-
-        resultado.push({
-          NomeEmpresa: "Kesh",
-          Loja: {
-            Descricao: "Kesh apresentação",
-            IdEmpresa: "809ee4ae-1748-49ed-b412-a597f05e65c9",
-            IdPerfilEmpresa: "3bfc170f-218c-4385-b45d-5ed2aaa366be",
-          } as Perfil,
-          Pontos: 50,
-          PontosEmDinheiro: 10
-        })
 
         this.pessoaLojas = resultado;
         this.pessoaLojas.sort(a => a.Pontos);
